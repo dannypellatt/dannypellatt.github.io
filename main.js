0e769da3aa3963f916f7f1a3e37ee5c1e41dbee0
dannypellatt <108154659+dannypellatt@users.noreply.github.com>
2023-05-22 14:54:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -32,48 +32,6 @@ $(document).ready(function(){
     }); 
 });
 
-// makes the category highlighted when chosen
-$('.radio_mark_category').on('click', function(){
-    $('.radio_mark_category').removeClass('selected');
-    $('.radio_mark_project').removeClass('selected');
-    $(this).addClass('selected');
-});
-$('.radio_mark_project').on('click', function(){
-    $('.radio_mark_project').removeClass('selected');
-    $(this).addClass('selected');
-});
-
-// Stops the carousel from sliding automatically
-$('.carousel').carousel({
-    interval: false,
-  });
-
-  // clones carousel caption below the image (still one on image, made opaque)
-  jQuery(function ($) {
-    $('.carousel').carousel();
-    var caption = $('div.carousel-item:nth-child(1) .carousel-caption');
-    $('.new-caption-area').html(caption.html());
-    caption.css('display', 'none');
-
-    $(".carousel").on('slide.bs.carousel', function (evt) {
-        var caption = $('div.carousel-item:nth-child(' + ($(evt.relatedTarget).index() + 1) + ') .carousel-caption');
-        $('.new-caption-area').html(caption.html());
-        caption.css('display', 'none');
-    });
-});
-
-
-var check;
-
-$('input[type="radio"]').hover(function() {
-    check = $(this).is(':checked');
-});
-
-$('input[type="radio"]').click(function() {
-    check = !check;
-    $(this).attr("checked", check);
-});
-
 
 
 // handles the modal for certs section
@@ -105,27 +63,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-
-
-
-
-//handles read more and less on mobile portfolio section
-$(".more").on("click", function(){
-  if($(this).text()=="Close")
-  {
-      $(this).text("Read More");
-  } else {
-      $(this).text("Close");
-  }
-  $(this).next('div').slideToggle();
-
-  return false;
-});
-
-
-
-
-
+// START navbar SECTION --------------------------------------------------------
 
 $(function () {
   $(document).scroll(function () {
@@ -134,21 +72,139 @@ $(function () {
 	});
 });
 
+// START portfolio SECTION -----------------------------------------------------
 
-
-
-// This is the toggle for roadmap section
-
-function openPath(evt, pathName) {
+function openProject(evt, projectName) {
+  // Declare all variables
   var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].pathName = tablinks[i].className.replace(" active", "");
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(pathName).style.display = "block";
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(projectName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var defaultEvent = {currentTarget: document.getElementsByClassName("tablinks")[0]};
+  openProject(defaultEvent, 'DataCo');
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+$(document).ready(function() {
+  var getProductHeight = $('.product.active').height();
+
+  $('.products').css({
+    height: getProductHeight
+  });
+
+  function calcProductHeight() {
+    getProductHeight = $('.product.active').height();
+
+    $('.products').css({
+      height: getProductHeight
+    });
+  }
+
+  var productItem = $('.product'),
+    productCurrentItem = productItem.filter('.active');
+
+  $('#next').on('click', function(e) {
+    e.preventDefault();
+
+    var nextItem = productCurrentItem.next();
+
+    productCurrentItem.removeClass('active');
+
+    if (nextItem.length) {
+
+      productCurrentItem = nextItem.addClass('active');
+    } else {
+      productCurrentItem = productItem.first().addClass('active');
+    }
+
+    calcProductHeight();
+  });
+
+  $('#prev').on('click', function(e) {
+    e.preventDefault();
+
+    var prevItem = productCurrentItem.prev();
+
+    productCurrentItem.removeClass('active');
+
+    if (prevItem.length) {
+      productCurrentItem = prevItem.addClass('active');
+    } else {
+      productCurrentItem = productItem.last().addClass('active');
+    }
+
+    calcProductHeight();
+  });
+
+
+  // ---- 'Puppy Tax modal' ----
+// Get the <a> tag that opens the modal
+var img = document.getElementById("banjoImg");
+
+// Get the image and insert it inside the modal
+var modalImg = document.getElementById("img01");
+
+img.onclick = function() {
+  modal.style.display = "block";
+  modalImg.src = "images/about/banjoOutside1.jpeg";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// --------
+
+  // Ripple
+  $('[ripple]').on('click', function(e) {
+    var rippleDiv = $('<div class="ripple" />'),
+      rippleSize = 60,
+      rippleOffset = $(this).offset(),
+      rippleY = e.pageY - rippleOffset.top,
+      rippleX = e.pageX - rippleOffset.left,
+      ripple = $('.ripple');
+
+    rippleDiv.css({
+      top: rippleY - (rippleSize / 2),
+      left: rippleX - (rippleSize / 2),
+      background: $(this).attr("ripple-color")
+    }).appendTo($(this));
+
+    window.setTimeout(function() {
+      rippleDiv.remove();
+    }, 1900);
+  });
+});
